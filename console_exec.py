959248feebdd7827b6871ee414bbb1a238daa40b
Jack Cherng <jfcherng@gmail.com>
2016-04-05 01:07:06 +0800

    Add the ability to open terminal depending on the Desktop Environment

--- console_exec.py
+++ console_exec.py
@@ -30,10 +30,13 @@ class ConsoleExecCommand(sublime_plugin.WindowCommand):
             pause = ['pause']
             console_cmd = console + cmd + ['&'] + pause
         else:
-            console = unix_console or ['xterm', '-e']
-            escaped_cmd = ' '.join(quote(x) for x in cmd)
-            pause = 'bash -c \'read -p "Press [Enter] to continue..."\''
-            console_cmd = console + ['{} ; {}'.format(escaped_cmd, pause)]
+            console = unix_console or self.get_unix_console()
+            pause = '; read -p "Press [Enter] to continue..."'
+            escaped_cmd = ' '.join([quote(x) for x in cmd])
+            console_cmd = console + ['bash -c ' + quote(escaped_cmd + pause)]
+
+        # debug
+        self.debug_print('reconstructed console_cmd is', console_cmd)
 
         # Default the to the current file's directory if no working directory
         # was provided
@@ -66,3 +69,28 @@ class ConsoleExecCommand(sublime_plugin.WindowCommand):
         finally:
             if old_path:
                 os.environ['PATH'] = old_path
+
+    def get_unix_console (self):
+        sessions = ['gnome-session', 'ksmserver', 'xfce4-session', 'lxsession']
+        ps = 'ps -eo comm | grep -E "{0}"'.format('|'.join(sessions))
+        # get the first found session or an empty string
+        session_found = os.popen(ps).read().split('\n')[0]
+        # Gnome
+        if session_found == 'gnome-session':
+            console = ['gnome-terminal', '-e']
+        # XDE
+        elif session_found == 'xfce4-session':
+            console = ['terminal', '-e']
+        # KDE
+        elif session_found == 'ksmserver':
+            console = ['konsole', '-e']
+        # LXDE
+        elif session_found == 'lxsession':
+            console = ['lxterminal', '-e']
+        # default
+        else:
+            console = ['xterm', '-e']
+        return console
+
+    def debug_print (self, *arg):
+        print('Console Exec:', *arg)
